f7781398d34872e0138b9292b48e9d62f229ed67
daxue <xuediwang@yufuid.com>
2020-09-16 13:00:53 +0800

    init app

---
+++ onLineTools/src/main/java/org/daxue/OnLineToolsApplication.java
@@ -0,0 +1,11 @@
+package org.daxue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnLineToolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnLineToolsApplication.class, args);
+    }
+}

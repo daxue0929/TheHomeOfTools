6644a38a3ef5a1fb752f746660cf6252030f5f47
daxue <xuediwang@yufuid.com>
2020-10-29 14:16:36 +0800

    [nacos-consumer][nacos-provider]初始化测试项目nacos

--- nacos-test/nacos-consumer/src/test/java/com/daxue/nacosconsumer/NacosConsumerApplicationTests.java
+++ nacos-test/nacos-consumer/src/test/java/com/daxue/nacosconsumer/NacosConsumerApplicationTests.java
@@ -1,13 +1,12 @@
 package com.daxue.nacosconsumer;
 
-import org.junit.jupiter.api.Test;
+
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NacosConsumerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }

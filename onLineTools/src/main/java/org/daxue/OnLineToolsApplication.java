b3380fd4ed4e7d28702bf335600c4daa38147d2a
daxue <xuediwang@yufuid.com>
2020-11-09 18:21:32 +0800

    [onLineTools] nacos遗留问题解决

--- onLineTools/src/main/java/org/daxue/OnLineToolsApplication.java
+++ onLineTools/src/main/java/org/daxue/OnLineToolsApplication.java
@@ -1,15 +1,24 @@
 package org.daxue;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class OnLineToolsApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnLineToolsApplication.class, args);
+    }
 
+    @Value("${servlet.port}")
+    public String port;
 
+    @Bean
+    public Object test() {
+        System.out.println("[daxue] current application is run on:" + port);
+        return new Object();
     }
 }
